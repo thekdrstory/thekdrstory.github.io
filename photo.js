86e103dfc95f41bf9f7b946da1ada35ded3b96ff
thekdrstory <82068638+thekdrstory@users.noreply.github.com>
2024-01-17 02:16:48 -0800

    Adding more photos

--- photo.js
+++ photo.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalButtons = [
         { button: document.getElementById('popup_portraits'), content: document.getElementById('info_portraits') },
         { button: document.getElementById('popup_landscapes'), content: document.getElementById('info_landscapes') },
+        { button: document.getElementById('popup_summermilkshake2023'), content: document.getElementById('info_summermilkshake2023') },
+        { button: document.getElementById('popup_perfect-man'), content: document.getElementById('info_perfect-man') },
     ];
 
     // Add event listeners to open the dialog when buttons are clicked
